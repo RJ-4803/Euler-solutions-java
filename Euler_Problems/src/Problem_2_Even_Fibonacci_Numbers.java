86dad21be5ead1587869e4f8300c55ec9b10de5e
RJ-4803 <renujaan4803@gmail.com>
2025-09-26 12:43:40 +0530

    fix: modify the files

--- Euler_Problems/src/Problem_2_Even_Fibonacci_Numbers.java
+++ Euler_Problems/src/Problem_2_Even_Fibonacci_Numbers.java
@@ -1,8 +1,19 @@
 public class Problem_2_Even_Fibonacci_Numbers {
     public static void main(String[] args) {
-        int x=0;
-        int y=1;
-        int z=x+y;
+        int x,y,z,sum;
+        sum=0;
+        x=0;
+        y=1;
+        for(int i=1;i<=4_000_000;i=z){
+            z=x+y;
+            //System.out.println(z);
+            if(z%2==0){
+                sum=sum + z;
+            }
+            x=y;
+            y=z;
+        }
+        System.out.println(sum);
 
 
     }
